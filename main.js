ba2edfd6b3773bf0528bf8749fc3d43a10a35f5a
Andy 8421さん <48783339+yichang8421@users.noreply.github.com>
2020-12-10 21:28:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-var div1 = document.createElement('div')
-div1.className = 'demo'
+var div1 = document.createElement('div');
+div1.className = 'demo';
 
-document.body.appendChild(div1)
+document.body.appendChild(div1);
 
-var dragging = false
-var initialX
-var initialY
+var dragging = false;
+var initialX;
+var initialY;
 
 div1.onmousedown = function (e) {
-    initialX = e.clientX
-    initialY = e.clientY
-    dragging = true
+    initialX = e.clientX;
+    initialY = e.clientY;
+    dragging = true;
 }
 
 document.onmousemove = function (e) {
     if (dragging === true) {
-        var deltaX = e.clientX - initialX
-        var deltaY = e.clientY - initialY
-        var top = parseInt(div1.style.top) || 0
-        var left = parseInt(div1.style.left) || 0
-        div1.style.top = top + deltaY + 'px'
-        div1.style.left = left + deltaX + 'px'
-        initialX = e.clientX
-        initialY = e.clientY
+        var deltaX = e.clientX - initialX;
+        var deltaY = e.clientY - initialY;
+        var top = parseInt(div1.style.top) || 0;
+        var left = parseInt(div1.style.left) || 0;
+        div1.style.top = top + deltaY + 'px';
+        div1.style.left = left + deltaX + 'px';
+        initialX = e.clientX;
+        initialY = e.clientY;
     }
 }
 
 document.onmouseup = function () {
-    dragging = false
+    dragging = false;
 }
 

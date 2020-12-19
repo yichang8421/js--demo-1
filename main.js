dfd389a16bb1f9e8016b899401c60812f5c1f735
Andy 8421さん <48783339+yichang8421@users.noreply.github.com>
2020-12-19 20:43:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var initialY;
 div1.onmousedown = function (e) {
     initialX = e.clientX;
     initialY = e.clientY;
+    
     dragging = true;
 }
 
@@ -17,10 +18,13 @@ document.onmousemove = function (e) {
     if (dragging === true) {
         var deltaX = e.clientX - initialX;
         var deltaY = e.clientY - initialY;
+        
         var top = parseInt(div1.style.top) || 0;
         var left = parseInt(div1.style.left) || 0;
+        
         div1.style.top = top + deltaY + 'px';
         div1.style.left = left + deltaX + 'px';
+        
         initialX = e.clientX;
         initialY = e.clientY;
     }
